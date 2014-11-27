3c0b159aa38b377794b11040239410c5556f8cb7
Adam Patterson <hello@adampatterson.ca>
2014-11-26 23:31:32 -0700

    Core starter, learning how packages wrok.

---
+++ src/Helm/Core/CoreServiceProvider.php
@@ -0,0 +1,45 @@
+<?php namespace Helm\Core;
+
+use Illuminate\Support\ServiceProvider;
+
+class CoreServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	public function boot()
+	{
+		$this->package('helm/core');
+
+		$this->bindRepositories();
+
+		require_once __DIR__.'/../../helpers.php';
+		require_once __DIR__.'/../../routes.php';
+		require_once __DIR__.'/../../filters.php';
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
